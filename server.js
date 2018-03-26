26b0d4446bda2f2468b773ac547f30c185bd6349
Glitch (timestamp-fcc-amirzamzam) <none>
2018-03-26 04:35:34 +0000

    🍶🐜 Checkpoint ./server.js:780437/233

--- server.js
+++ server.js
@@ -6,16 +6,19 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var cors = require('cors')
 
+
+var app = module.exports = express();
+
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-var app = module.exports = express();
+
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/dateValues', function(req,res,next){
 
-  
+console.log("It Works");
   
 });
 
